a848bbd1f92c6602e8586f55f63083708e63478c
Myron <bruscel96@gmail.com>
2024-11-16 18:05:38 -0500

    adding evironment logger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'client';
+
+  constructor() {
+    if (environment.production) {
+      console.log('Running in production mode');
+    } else {
+      console.log('Running in development mode');
+    }
+  }
 }
 
 
